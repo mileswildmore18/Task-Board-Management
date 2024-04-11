6683f0201c6617ca9ad95146b953636a7edd1edf
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-11 10:07:18 -0400

    Added comments to the JavaScript with the process of what the news tasks are doing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 // Retrieve tasks and nextId from localStorage
+// Inputs imported from the HTML
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 const addTaskButton = $('.btn-success');
 const modal = $('.modal');
+const taskTitleInput = document.querySelector("task-title");
+const dueDateInput = document.querySelector("date");
+const descriptionInput = document.querySelector("description");
 
 // Todo: create a function to generate a unique task id
+//Give the tasks a unique ID
 function generateTaskId() {
     const id = Math.random().toString(36).substr(2, 9);
     console.log(id);
 }
 
 // Todo: create a function to create a task card
+// Create a task card with the information
 function createTaskCard(task) {
     let div = $('<div>');
     div.addClass('card draggable').attr("style", "width: 18rem");
@@ -23,11 +29,13 @@ function createTaskCard(task) {
     div.append(cardBody);
     cardBody.append(cardDate);
     cardBody.append(cardInfo);
-    
+
+    return task;
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
+// Make the tasks draggable
 function renderTaskList() {
     $( function() {
         $( "#draggable" ).draggable();
@@ -35,18 +43,21 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
+// Adds a new task
 function handleAddTask(event){
-    let task = document.querySelector("modal-form");
-  let inputVal = document.getElementById("task").value;
+    let task = document.querySelector("modal-content");
+  let inputVal = document.getElementsByClassName("modal-form").value;
   task.innerHTML = inputVal;
 }
 
 // Todo: create a function to handle deleting a task
+// This function will delete the task
 function handleDeleteTask(event){
 
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
+//Makes the task droppable to the given box
 function handleDrop(event, ui) {
     $( function() {
         $( "#draggable" ).draggable();
@@ -65,25 +76,17 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
+    //Clears the Modal when "Add Task" is clicked
+$(".btn-success").on("click", function (){
+    const chosenTask = JSON.parse(localStorage.getItem ("tasks")) || [];
+    const newTask = {
+        //Adds the values of information put in the Modal task box
+        title:taskTitleInput.val(),
+        date:dueDateInput.val(),
+        description:descriptionInput.val()
+    }
+    chosenTask.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(chosenTask));
+})
+
 });
-
-
-{/* <div class="modal" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Add Task</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <p>Task Title</p>
-            <p>Task Due Date</p>
-            <p>Task Description</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div> */}
