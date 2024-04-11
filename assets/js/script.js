2264f18dcebbd066f615958707799590fbb99a05
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-11 19:01:39 -0400

    Added the variables to bring information added to the Task Modal Bar into the array of information submitted by the user along with the todo cards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,85 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 const addTaskButton = $('.btn-success');
 const modal = $('.modal');
-const taskTitleInput = document.querySelector("task-title");
-const dueDateInput = document.querySelector("date");
-const descriptionInput = document.querySelector("description");
+const taskTitleInput = document.querySelector(".task-title");
+const dueDateInput = document.querySelector(".date");
+const descriptionInput = document.querySelector(".task");
+
+function formSubmit(){
+    const taskArray = JSON.parse(localStorage.getItem('strings')) || [];
+
+    const taskObject = {
+      //add generate task id function
+      objectTitle: title.value,
+      objectDate: date.value,
+      objectDescription: description.value.trim()
+    }
+
+    taskArray.push(taskObject);
+    localStorage.setItem('tasks', JSON.stringify(taskArray));
+    
+    $("#todo-cards").append(createTaskCard(taskObject));
+}
 
 // Todo: create a function to generate a unique task id
 //Give the tasks a unique ID
 function generateTaskId() {
+  
     const id = Math.random().toString(36).substr(2, 9);
     console.log(id);
 }
+//Add cards to the box
 
 // Todo: create a function to create a task card
 // Create a task card with the information
 function createTaskCard(task) {
-    let div = $('<div>');
-    div.addClass('card draggable').attr("style", "width: 18rem");
-    let title = $("div").addClass("task-title").text("task-title");
-    let cardBody = $("div").addClass("modal-form");
-    let cardDate = $("due-date").addClass("date").text('date');
-    let cardInfo = $("description").addClass("task").text("description");
+  console.log(task);
+  //Add a button for deleting
+  // When delete is clicked, it deletes the box
+    const taskCard = $('<div>')
+    .addClass('card project-card draggable my-3')
+    .attr('data-project-id', project.id);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.objectTitle);
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.objectDescription);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.objectDate);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-project-id', task.id);
+  cardDeleteBtn.on('click', handleDeleteTask);
 
-    div.append(title);
-    div.append(cardBody);
-    cardBody.append(cardDate);
-    cardBody.append(cardInfo);
+  // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+  //if (task.objectDate && project.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.objectDate, 'DD/MM/YYYY');
 
-    return task;
+
+  //If the task is due today, make the card yellow. If it is overdue, make it red.
+  if (now.isSame(task.objectDate, 'day')) {
+    taskCard.addClass('bg-warning text-white');
+  } else if (now.isAfter(task.objectDate)) {
+    taskCard.addClass('bg-danger text-white');
+    cardDeleteBtn.addClass('border-light');
+  }
+
+ // Gather all the elements created above and append them to the correct elements.
+cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+taskCard.append(cardHeader, cardBody);
+
+//Return the card so it can be appended to the correct lane.
+console.log("Items entered")
+    return taskCard;
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 // Make the tasks draggable
 function renderTaskList() {
+   createTaskCard();
+    
+    //Calls Create the task card
+    //Make it draggable and droppable
     $( function() {
         $( "#draggable" ).draggable();
       } );
@@ -45,6 +91,7 @@ function renderTaskList() {
 // Todo: create a function to handle adding a new task
 // Adds a new task
 function handleAddTask(event){
+    renderTaskList();
     let task = document.querySelector("modal-content");
   let inputVal = document.getElementsByClassName("modal-form").value;
   task.innerHTML = inputVal;
@@ -53,7 +100,10 @@ function handleAddTask(event){
 // Todo: create a function to handle deleting a task
 // This function will delete the task
 function handleDeleteTask(event){
-
+    
+    //Delete the box when Delete is clicked
+    //Rerender the box using the Render Task Card
+    
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
@@ -76,17 +126,23 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
-    //Clears the Modal when "Add Task" is clicked
-$(".btn-success").on("click", function (){
+    //Clears the Modal when "Add Task" is clicked 
+    //add LocalStorage
+    //call the render task list
+    
+$("#add-task").on("click", function (){
+    console.log('I clicked the button');
     const chosenTask = JSON.parse(localStorage.getItem ("tasks")) || [];
+    console.log($(".task-title").val());
     const newTask = {
         //Adds the values of information put in the Modal task box
-        title:taskTitleInput.val(),
-        date:dueDateInput.val(),
-        description:descriptionInput.val()
+        title:$(".task-title").val(),
+        date:$(".date").val(),
+        description:$(".task").val()
     }
     chosenTask.push(newTask);
     localStorage.setItem("tasks", JSON.stringify(chosenTask));
+
 })
 
 });
